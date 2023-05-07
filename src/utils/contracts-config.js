5f3a11309f5b8fb0ac79c4e75cf21881f955bbaf
aravindthonupunuri <aravindthonupunuri75@gmail.com>
2023-05-06 23:02:20 -0700

    vacation rental development

---
+++ src/utils/contracts-config.js
@@ -0,0 +1,5 @@
+
+      export const contractAddress = "0xba009d4f4a3567D88BADb0aBDCCF396a69AEBb89"
+      export const ownerAddress = "0x2c5Cd384291d2054A1F211F150Cec82533CB13Dc"
+      export const networkDeployedTo = "11155111"
+    
